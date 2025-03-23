2b36de315f7fdbc7651011f5238e84eaf4f85b3f
SANSKAR <sanskar32lke@gmail.com>
2025-03-23 19:37:25 +0530

    home setup fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SignUp from "./Components/SignUp";
 import NoteState from "./Context/Notes/NoteState";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Account from "./Components/Account";
-import Learner from "./Components/Learner,jsx";
+import Learner from "./Components/Learner.jsx";
 
 function App() {
   return (
